ac38f659433cc537d6a38a7e852b7c8cbdc6f5d0
Peter Mulholland <pmulholland08@gmail.com>
2021-01-14 21:42:43 -0500

    Fix tour modal preventing click and drag

--- src/components/ui/tours/TourModal.tsx
+++ src/components/ui/tours/TourModal.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { setTourModalOpen } from "../../../redux/actionCreators";
 import { RootState } from "../../../redux/store";
 import { connect, ConnectedProps } from "react-redux";
-import { useSpring, animated } from "react-spring";
+import { useSpring, animated, useTransition } from "react-spring";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { useTranslation } from "react-i18next";
@@ -22,29 +22,40 @@ const connector = connect(mapStateToProps, mapDispatchToProps);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
 export const TourModal = connector(({ tourModalOpen, setTourModalOpen }: PropsFromRedux) => {
-  const props = useSpring({ opacity: tourModalOpen ? 1 : 0 });
+  const transitions = useTransition(tourModalOpen, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
   const { t } = useTranslation();
   return (
-    <animated.div style={props} className="tour-modal-container">
-      <div className="tour-modal">
-        <div className="tour-modal-header">
-          <h3>{t("tours")}</h3>
-          <button onClick={() => setTourModalOpen(false)} className="close-button">
-            <FontAwesomeIcon icon={faTimes} size={"2x"} />
-          </button>
-        </div>
-        {tours.map((tour) => (
-          <button
-            className="tour"
-            onClick={() => {
-              setTourModalOpen(false);
-              tour.startTour();
-            }}
-          >
-            {tour.name}
-          </button>
-        ))}
-      </div>
-    </animated.div>
+    <div>
+      {transitions.map(
+        ({ item, key, props }) =>
+          item && (
+            <animated.div key={key} style={props} className="tour-modal-container">
+              <div className="tour-modal">
+                <div className="tour-modal-header">
+                  <h3>{t("tours")}</h3>
+                  <button onClick={() => setTourModalOpen(false)} className="close-button">
+                    <FontAwesomeIcon icon={faTimes} size={"2x"} />
+                  </button>
+                </div>
+                {tours.map((tour) => (
+                  <button
+                    className="tour"
+                    onClick={() => {
+                      setTourModalOpen(false);
+                      tour.startTour();
+                    }}
+                  >
+                    {tour.name}
+                  </button>
+                ))}
+              </div>
+            </animated.div>
+          )
+      )}
+    </div>
   );
 });
